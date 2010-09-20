756cc8eb7d0c563ff161f826c581dd6593349189
Tony Morris <code@tmorris.net>
2010-09-20 13:41:38 +1000

    Added JFrame

--- src/main/scala/scalaz/Main.scala
+++ src/main/scala/scalaz/Main.scala
@@ -1,8 +1,11 @@
 package scalaz
 
+import javax.swing.JFrame
+
 object Main {
   def main(args: Array[String]) {
     println("scalaz-depend")
+    new JFrame("test") setVisible true
   }
 
   val f: Int => String = _ => "abc"
