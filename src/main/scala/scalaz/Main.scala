c04d99b286b4abf8044e086702ccd4c89073b05b
Tony Morris <code@tmorris.net>
2010-10-25 16:09:44 +1000

    dispose frame on exit

--- src/main/scala/scalaz/Main.scala
+++ src/main/scala/scalaz/Main.scala
@@ -1,11 +1,14 @@
 package scalaz
 
 import javax.swing.JFrame
+import javax.swing.WindowConstants.DISPOSE_ON_CLOSE
 
 object Main {
   def main(args: Array[String]) {
     println("scalaz-depend")
-    new JFrame("test") setVisible true
+    val j = new JFrame("test")
+    j setDefaultCloseOperation DISPOSE_ON_CLOSE
+    j setVisible true
   }
 
   val f: Int => String = _ => "abc"
